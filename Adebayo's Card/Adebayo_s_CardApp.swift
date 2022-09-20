ef2e1753170c83707b59657b92084e186c50e6a3
Adebayo Sotannde <tsotannde@icloud.com>
2022-09-20 11:34:51 -0400

    Initial Commit

---
+++ Adebayo's Card/Adebayo_s_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adebayo_s_CardApp.swift
+//  Adebayo's Card
+//
+//  Created by Adebayo Sotannde on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Adebayo_s_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
